de2c9b0cdb6600cd838e560223a4aa1b46e3d7ab
Smoking Bellpepper <43789406+SmokingBellpepper@users.noreply.github.com>
2018-10-17 19:11:01 +0900

    Change .png → .jpg
    I changed the file extension from ".png" to ".jpg".

--- change_background.js
+++ change_background.js
@@ -1,13 +1,13 @@
 var list = 
 [
-    "url('./image/1.png')",
-    "url('./image/2.png')",
-    "url('./image/3.png')",
-    "url('./image/4.png')",
-    "url('./image/5.png')",
-    "url('./image/6.png')",
-    "url('./image/7.png')",
-    "url('./image/8.png')",
+    "url('./image/1.jpg')",
+    "url('./image/2.jpg')",
+    "url('./image/3.jpg')",
+    "url('./image/4.jpg')",
+    "url('./image/5.jpg')",
+    "url('./image/6.jpg')",
+    "url('./image/7.jpg')",
+    "url('./image/8.jpg')",
 ]
 
 var choice = list[Math.floor(Math.random()*list.length)];
